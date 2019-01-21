573e2180723f75f68ed91773376c43490f0db656
Jevin Sweval <jevinsweval@gmail.com>
2019-01-21 09:00:27 -0500

    same python so imports in stub as real thing

--- lib/fli/fli_stub.c
+++ lib/fli/fli_stub.c
@@ -35,6 +35,17 @@ void ctor(void) {
     Py_Initialize();
     ret = PyRun_SimpleString("from time import time,ctime\n"
     					"import sys\n"
+                        "import math\n"
+                        "import ctypes\n"
+                        "import xml.parsers.expat\n"
+                        "import xml.etree.ElementTree\n"
+                        "import heapq\n"
+                        "import hashlib\n"
+                        "import os\n"
+                        "import random\n"
+                        "import mmap\n"
+                        "import _opcode\n"
+                        "print(os.getpid())\n"
                        "print('Today is', ctime(time()), file=sys.stderr, flush=True)\n");
     fprintf(stderr, "simplestring() = %d\n", ret);
     // self_handle = dlopen("/home/jevin/code/hdl/cocotb/git/cocotb/build/libs/i686/libfli.so", RTLD_LAZY | RTLD_GLOBAL);
@@ -47,7 +58,8 @@ void dtor(void) {
 	int ret;
     fprintf(stderr, "fli stub dtor\n");
     // ret = pyfin();
-    ret = Py_FinalizeEx();
+    // ret = Py_FinalizeEx();
+    ret = 242;
     fprintf(stderr, "Py_FinalizeEx() = %d\n", ret);
     // ret = dlclose(py_handle);
     ret = 243;
